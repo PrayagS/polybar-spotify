2dc8be7b323201ad1e6b2e3575a11b04091f92cc
PrayagS <prayag.s@ahduni.edu.in>
2020-06-08 17:31:16 +0530

    Update indentation in get_spotify_status.sh

--- get_spotify_status.sh
+++ get_spotify_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if	[ "$(playerctl status spotify)" = "Stopped" ]; then
-  echo "No music is playing"
+    echo "No music is playing"
 else # Can be configured to output differently when player is paused
 	echo "$(playerctl metadata spotify --format "{{ title }} - {{ artist }}")"
 fi
