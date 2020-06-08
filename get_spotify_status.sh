bfe9d4bc0970e6294ed971d48c78cfe975197ca0
Prayag Savsani <prayag.s@ahduni.edu.in>
2020-06-08 17:31:50 +0530

    Update get_spotify_status.sh

--- get_spotify_status.sh
+++ get_spotify_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if	[ "$(playerctl status spotify)" = "Stopped" ]; then
+if [ "$(playerctl status spotify)" = "Stopped" ]; then
     echo "No music is playing"
 else # Can be configured to output differently when player is paused
-	echo "$(playerctl metadata spotify --format "{{ title }} - {{ artist }}")"
+    echo "$(playerctl metadata spotify --format "{{ title }} - {{ artist }}")"
 fi
