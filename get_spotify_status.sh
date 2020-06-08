978e30b5d0ec57e5f632c79bc6cc6cc24b47547c
PrayagS <prayag.s@ahduni.edu.in>
2020-06-08 18:36:38 +0530

    Passes shellcheck tests

--- get_spotify_status.sh
+++ get_spotify_status.sh
@@ -3,5 +3,5 @@
 if [ "$(playerctl status spotify)" = "Stopped" ]; then
     echo "No music is playing"
 else # Can be configured to output differently when player is paused
-    echo "$(playerctl metadata spotify --format "{{ title }} - {{ artist }}")"
+    playerctl metadata spotify --format "{{ title }} - {{ artist }}"
 fi
--- scroll_spotify_status.sh
+++ scroll_spotify_status.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-zscroll -l 20 \ # the length of the text window that is scrolled
-        --delay 0.3 \ # the scroll delay
-        # see man zscroll for documentation of the following commands
+# see man zscroll for documentation of the following parameters
+zscroll -l 20 \
+        --delay 0.3 \
         --match-command "playerctl status spotify" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
