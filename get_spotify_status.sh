afae13ee890601bf585f045875e45b61bbb997cb
PrayagS <prayag.s@ahduni.edu.in>
2020-06-10 18:59:11 +0530

    Use playerctld to fetch the most recently used player instead of hard-coding spotify

--- get_spotify_status.sh
+++ get_spotify_status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ "$(playerctl status spotify)" = "Stopped" ]; then
+if [ "$(playerctl --player=playerctld status)" = "Stopped" ]; then
     echo "No music is playing"
-elif [ "$(playerctl status spotify)" = "Paused"  ]; then
+elif [ "$(playerctl --player=playerctld status)" = "Paused"  ]; then
     polybar-msg -p "$(pgrep -f "polybar bottom")" hook spotify-play-pause 2 >/dev/null
-    playerctl metadata spotify --format "{{ title }} - {{ artist }}"
-else 
+    playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
+else # Can be configured to output differently when player is paused
     polybar-msg -p "$(pgrep -f "polybar bottom")" hook spotify-play-pause 1 > /dev/null
     playerctl metadata spotify --format "{{ title }} - {{ artist }}"
 fi
