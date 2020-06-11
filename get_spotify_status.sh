8c0881fc92fe586f05fd9ff741b7ffb015175ce8
PrayagS <prayag.s@ahduni.edu.in>
2020-06-11 22:36:42 +0530

    Remove all occurences of the word spotify. Maybe I should change the repo name as well

--- get_spotify_status.sh
+++ get_spotify_status.sh
@@ -3,9 +3,9 @@
 if [ "$(playerctl --player=playerctld status)" = "Stopped" ]; then
     echo "No music is playing"
 elif [ "$(playerctl --player=playerctld status)" = "Paused"  ]; then
-    polybar-msg -p "$(pgrep -f "polybar bottom")" hook spotify-play-pause 2 >/dev/null
+    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 2 >/dev/null
     playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
 else # Can be configured to output differently when player is paused
-    polybar-msg -p "$(pgrep -f "polybar bottom")" hook spotify-play-pause 1 > /dev/null
-    playerctl metadata spotify --format "{{ title }} - {{ artist }}"
+    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 1 >/dev/null
+    playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
 fi
--- scroll_spotify_status.sh
+++ scroll_spotify_status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # see man zscroll for documentation of the following parameters
-zscroll -l 20 \
-        --delay 0.3 \
-        --match-command "playerctl status spotify" \
+zscroll -l 30 \
+        --delay 0.1 \
+        --match-command "playerctl --player=playerctld status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
         --update-check true '/home/prayag_s/.config/polybar/scripts/get_spotify_status.sh' &
