30de6d46acfd18e2f07432d70df1de6438fcdb2d
Joe King <kmahadew21@gmail.com>
2020-10-17 19:31:48 +0200

    redirect polybar-msg stderr to /dev/null

--- get_spotify_status.sh
+++ get_spotify_status.sh
@@ -3,9 +3,9 @@
 if [ "$(playerctl --player=playerctld status)" = "Stopped" ]; then
     echo "No music is playing"
 elif [ "$(playerctl --player=playerctld status)" = "Paused"  ]; then
-    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 2 >/dev/null
+    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 2 1>/dev/null 2>&1
     playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
 else # Can be configured to output differently when player is paused
-    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 1 >/dev/null
+    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 1 1>/dev/null 2>&1
     playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
 fi
