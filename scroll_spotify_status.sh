00875a4647d2177c451f2f9412a168f95dcc651c
SeitaHigashi <higashi110902@gmail.com>
2021-08-01 20:12:47 +0900

    Update file reference method
    The reference to get_spotify_status for scroll_spotify_status.sh is now a relative reference

--- scroll_spotify_status.sh
+++ scroll_spotify_status.sh
@@ -4,9 +4,10 @@
 zscroll -l 30 \
         --delay 0.1 \
         --scroll-padding "  " \
-        --match-command "$HOME/.config/polybar/scripts/get_spotify_status.sh --status" \
+        --match-command "`dirname $0`/get_spotify_status.sh --status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
-        --update-check true "$HOME/.config/polybar/scripts/get_spotify_status.sh" &
+        --update-check true "`dirname $0`/get_spotify_status.sh" &
 
 wait
+
